60159a85ae75e8379b41aad6e8d50c2f075397dd
krushna <krushnamk1009@gmail.com>
2024-03-31 21:18:13 +0530

    push todo basic project

---
+++ projects/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+<TodoList></TodoList>
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+export default {
+  components: {
+    TodoList,
+  },
+};
+</script>
---
+++ projects/src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from "vue";
+
+const newTask = ref("");
+const taskList = ref([]);
+
+function addTask() {
+  if (newTask.value.trim() !== "") {
+    taskList.value.push(newTask.value);
+    newTask.value = "";
+  }
+}
+
+function removeTask(index) {
+  taskList.value.splice(index, 1);
+}
+</script>
+
+<template>
+  <div>
+    <h1>TodoList</h1>
+
+    <div>
+      <!-- Removed : before v-for -->
+      <input
+        type="text"
+        placeholder="enter task"
+        @keyup.enter="addTask"
+        v-model="newTask"
+      />
+      <button @click.prevent="addTask">Add</button>
+    </div>
+
+    <div>
+      <!-- Removed : before v-for and added :key="index" -->
+      <ul v-for="(task, index) in taskList" :key="index">
+        <li>{{ task }}</li>
+        <button @click.prevent="removeTask(index)">Remove</button>
+      </ul>
+    </div>
+  </div>
+</template>
